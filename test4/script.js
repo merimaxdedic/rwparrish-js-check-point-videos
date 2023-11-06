5e235681b3b7cf1dd9486e10a59da01469fa619c
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-11-06 14:14:25 -0700

    add h1 with test number to each html file

--- test4/script.js
+++ test4/script.js
@@ -1,9 +1,11 @@
 /*
-Declare a variable called pizza and assign it a string value of a pizza type of your choosing e.g., "cheese" - although it should probably be "peperoni".
+Declare a variable called pizza and assign it a string value of a pizza type of your choosing e.g., "cheese" - ( (although it should probably be pepperoni ;)).
 
-Next, declare a function called checkNumber that takes a number as an argument and returns the string "positive" if the argument is greater than 0 and "negative" if the argument is less than 0.
+Next, declare a function called checkNumber that takes a number as an argument and returns the string "positive" if the argument is greater than 0 and "negative" if the argument is less than 0. What if the argument is 0? Add an else condition that returns "zero".
 
-Then test the function to be sure that it works.
+Then, test the function to be sure that it works. Open the html file and use the DevTools in the browser to display the output in the console.
+
+Did you use const or let to declare the pizza variable? What is the difference between the two keywords?
 
 Lastly, explain the scope of your function parameter and the pizza variable. 
 */
